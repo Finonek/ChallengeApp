b3fc60a07c047ffac8d7da8cdb27d59ed0ff5480
Bartek Kijowski <bakijowski@gmail.com>
2023-11-12 11:52:57 +0100

    Zadanie 4 08.11.23

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var myName = "Ewa";
+var myAge = 33;
+var mySex = "female";
+
+if (myName == "Ewa" && myAge == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (mySex == "female" && myAge < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (mySex == "male" && myAge < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (mySex == "female" && myAge > 30)
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
+}
+else if (myName == "Ewa" && mySex == "female")
+{
+    Console.WriteLine("Ewa to kobieta");
+}
